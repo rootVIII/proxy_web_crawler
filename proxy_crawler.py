ff3df278a9ab53bee96a4c1c69b12b5611e6dc5f
rootVIII <colleyloyejames@gmail.com>
2019-05-12 04:06:33 -0400

    added argparse

--- proxy_crawler.py
+++ proxy_crawler.py
@@ -1,39 +1,44 @@
 #! /usr/bin/python3
-# roboshout.com
-from sys import argv, exit
+from sys import exit
 from time import sleep
 from selenium import webdriver
+from pyvirtualdisplay import Display
 from selenium.webdriver.common.proxy import *
 from selenium.webdriver.common.keys import Keys
-import random
-import re
+from re import findall, sub
+from argparse import ArgumentParser
+from random import randint, random
+from requests import get
 
 
 # Search for a given website by a string of text
-# using a different proxy each iteration. Page indexes are
-# printed to the console (including final index of the site).
+# using a different proxy each iteration.
+# Page indexes are printed to the console including
+# final index of the site.
 # Once the site is found, a random link on the site
-# will be clicked
+# will be clicked.
 class ProxyCrawler:
-    def __init__(self, URL, keyword, socket_list=[]):
-        self.__scrape_socket()
+    def __init__(self, url, keyword):
+        self.socket_list = []
         self.keyword = keyword
-        self.url = URL
+        self.url = url
+        self.scrape_socket()
 
-    # get a list of sockets from https://www.sslproxies.org
-    def __scrape_socket(self):
+    # Get a list of sockets from https://www.sslproxies.org.
+    # Use http lib/requests here if desired.
+    def scrape_socket(self):
         string_builder = ""
-        browser1 = webdriver.Firefox()
-        browser1.get("https://www.sslproxies.org/")
-        sleep(2)
-        td_tags = browser1.find_elements_by_tag_name('td')
+        temp_browser = webdriver.Firefox()
+        temp_browser.get("https://www.sslproxies.org/")
+        td_tags = temp_browser.find_elements_by_tag_name('td')
         for tag in td_tags:
             string_builder += tag.text + " "
-        browser1.quit()
-        self.socket_list = re.findall("\d+.\d+.\d+.\d+\s+\d+", string_builder)
+        temp_browser.quit()
+        pattern = r"\d+.\d+.\d+.\d+\s+\d+"
+        self.socket_list = findall(pattern, string_builder)
 
-    # search with the given proxy. If an exception occurs,
-    # the loop continues and the next proxy is used.
+    # Search with the given proxy.
+    # If an exception occurs, loop continues and the next proxy is used.
     def start_search(self):
         for socket in self.socket_list:
             temp_socket = socket.split()
@@ -47,96 +52,107 @@ class ProxyCrawler:
             fp.set_preference("network.proxy.ssl_port", int(PROXY_PORT))
             fp.set_preference("general.useragent.override", "whater_useragent")
             fp.update_preferences()
-            browser2 = webdriver.Firefox(firefox_profile=fp)
+            browser = webdriver.Firefox(firefox_profile=fp)
             try:
-                browser2.get('https://www.bing.com/')
-                assert "Bing" in browser2.title
-            except:
+                browser.get('https://www.bing.com/')
+                assert "Bing" in browser.title
+            except Exception:
                 print("retrying Bing with different socket")
-                browser2.quit()
+                browser.quit()
                 continue
-            print("\n--------------------------------------------")
             print("using socket: " + PROXY_HOST + ":" + PROXY_PORT)
             print("searching for keyword(s):   " + self.keyword)
-            search_box = browser2.find_element_by_name("q")
-            sub_button = browser2.find_element_by_name("go")
-            sleep(random.randint(20, 30) + random.random())
+            search_box = browser.find_element_by_name("q")
+            sub_button = browser.find_element_by_name("go")
+            sleep(randint(20, 30) + random())
             search_box.send_keys(self.keyword)
-            sleep(random.randint(20, 30) + random.random())
+            sleep(randint(20, 30) + random())
             sub_button.send_keys(Keys.RETURN)
-            sleep(random.randint(20, 30) + random.random())
+            sleep(randint(20, 30) + random())
             page_index = 0
-            extractor = re.sub(r".*//", "", self.url)
+            extractor = sub(r".*//", "", self.url)
             if "www." in extractor:
                 domain = extractor[4:]
             else:
                 domain = extractor
-            while domain not in browser2.current_url:
+            while domain not in browser.current_url:
                 page_index += 1
                 print("current index:   " + str(page_index))
-                page_links = browser2.find_elements_by_xpath("//a[@href]")
+                page_links = browser.find_elements_by_xpath("//a[@href]")
                 found_link = ""
                 for link in page_links:
                     try:
                         if domain in link.get_attribute("href"):
                             sleep(1)
                             found_link = link
-                    except:
+                    except Exception:
                         print("stale element")
-                        browser2.quit()
+                        browser.quit()
                         break
                 if found_link:
                     print("Found " + domain + " at index " + str(page_index))
                     found_link.click()
-                sleep(5 + random.random())
+                sleep(5 + random())
                 try:
-                    if domain not in browser2.current_url:
-                        sleep(random.randint(5, 10) + random.random())
+                    if domain not in browser.current_url:
+                        sleep(randint(5, 10) + random())
                         try:
                             idx = str(page_index + 1)
-                            browser2.find_element_by_link_text(idx).click()
-                        except:
+                            browser.find_element_by_link_text(idx).click()
+                        except Exception:
                             print("Exception occurred: trying next socket")
-                            browser2.quit()
+                            browser.quit()
                             break
                     else:
                         page_index = 0
-                except:
+                except Exception:
                     print("Exception occurred: trying next socket")
-                    browser2.quit()
+                    browser.quit()
                     break
-            sleep(random.randint(30, 60) + random.random())
+            sleep(randint(30, 60) + random())
             try:
-                target_links = browser2.find_elements_by_xpath("//a[@href]")
-            except:
-                browser2.quit()
+                target_links = browser.find_elements_by_xpath("//a[@href]")
+            except Exception:
+                browser.quit()
                 continue
-            random_page_num = random.randint(0, len(target_links) - 1)
-            sleep(random.randint(30, 60) + random.random())
+            random_page_num = randint(0, len(target_links) - 1)
+            sleep(randint(30, 60) + random())
             target_link = target_links[random_page_num]
             try:
                 target_link.click()
-            except:
+            except Exception:
                 print("Invalid target link... retrying with next socket")
-                browser2.quit()
-                sleep(1 + random.random())
+                browser.quit()
+                sleep(1 + random())
                 continue
-            sleep(random.randint(10, 15) + random.random())
-            print("visiting random page:   " + browser2.current_url)
-            sleep(random.randint(30, 60) + random.random())
-            browser2.quit()
+            sleep(randint(10, 15) + random())
+            print("visiting random page:   " + browser.current_url)
+            sleep(randint(30, 60) + random())
+            browser.quit()
 
 
 if __name__ == "__main__":
-    if len(argv) < 3:
-        print('USAGE:')
-        print('python3 https://somesite.com keyword1 keyword2 ...')
-        print('Pass a complete URL and at least 1 search keyword')
-        exit()
-    if argv[1][:7] != "http://" and argv[1][:8] != "https://":
-        print("invalid URL... exiting")
-        exit()
-    keywd = ' '.join(argv[2:])
-    bot = ProxyCrawler(argv[1], keywd)
-    while True:
-        bot.start_search()
+    description = 'Usage: python proxy_crawler.py -u <https://example.com> '
+    description += '-k <search keyword> -d (optional - run with display)'
+    parser = ArgumentParser(description=description)
+    parser.add_argument('-u', '--url', required=True, help='url')
+    parser.add_argument('-k', '--keyword', required=True, help='keyword')
+    parser.add_argument('-d', '--display', help='display', action='store_true')
+    d = parser.parse_args()
+    try:
+        req = get(d.url)
+    except Exception as e:
+        print(e)
+        exit(1)
+    if req.status_code != 200:
+        print('Invalid URL')
+        exit(1)
+    if not d.display:
+        with Display():
+            while True:
+                bot = ProxyCrawler(d.url, d.keyword)
+                bot.start_search()
+    else:
+        while True:
+            bot = ProxyCrawler(d.url, d.keyword)
+            bot.start_search()
