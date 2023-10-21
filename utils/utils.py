08ab29d798eaa5d1ebbd54371c3f82bac8301b96
rootVIII <colleyloyejames@gmail.com>
2023-10-21 11:31:35 -0400

    line breaks in arg parse

--- utils/utils.py
+++ utils/utils.py
@@ -7,13 +7,16 @@ def get_cli_args():
     description = 'Usage: python proxy_crawler.py -u '
     description += '<https://example.com> -k <search keyword>'
     parser = ArgumentParser(description=description)
-    parser.add_argument(
-        '-u', '--url', required=True, help='Target URL including https://')
-    parser.add_argument(
-        '-k', '--keyword', required=True, help='keyword(s) to search for')
-    parser.add_argument(
-        '-x', '--headless', required=False,
-        action='store_true', help='requires pyvirtualdisplay & Linux OS')
+    parser.add_argument('-u', '--url',
+                        required=True,
+                        help='Target URL including https://')
+    parser.add_argument('-k', '--keyword',
+                        required=True,
+                        help='keyword(s) to search for')
+    parser.add_argument('-x', '--headless',
+                        required=False,
+                        action='store_true',
+                        help='requires pyvirtualdisplay & Linux OS')
     return parser.parse_args()
 
 
